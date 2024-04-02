3d8b1a0a1e76fd00ae1d72e1846631c8efe3111a
JermieTapang <jermietapang96@gmail.com>
2024-04-02 16:10:55 +0400

    Exercise-5-Embedded Content

---
+++ Chapter-5-Embedded Content/Exercises/Exercise- Embedded Content/sound.js
@@ -0,0 +1,23 @@
+const sounds = ['ah-ha', 'back-of-the-net', 'bangoutoforder', 'dan', 'emailoftheevening', 'hellopartridge', 'iateascothegg', 'imconfused'];
+
+sounds.forEach(sound => {
+    const btn = document.createElement('button');
+    btn.classList.add('btn');
+    btn.innerText = sound;
+
+    btn.addEventListener('click', () => {
+        stopPlaying();
+        document.getElementById(sound).play();
+    })
+
+    document.getElementById('buttons').append(btn);
+})
+
+
+function stopPlaying() {
+    sounds.forEach(sound => {
+        const audio = document.getElementById(sound);
+        audio.pause();
+        audio.currentTime = 0;
+    })
+}
