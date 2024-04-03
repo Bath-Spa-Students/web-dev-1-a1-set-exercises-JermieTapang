89182a237e7839c6f84284e7433d08504ca7382a
JermieTapang <jermietapang96@gmail.com>
2024-04-03 14:46:14 +0400

    Chapter6-Petrol-Calculator

---
+++ Chapter-6-Javascript/Exercises/Petrol-Calculator/petrol.js
@@ -0,0 +1,13 @@
+    // JavaScript code
+    function calculatePetrolCost() {
+        var distance = parseFloat(document.getElementById('distance').value);
+        var fuelPrice = parseFloat(document.getElementById('fuelPrice').value);
+        
+        if (isNaN(distance) || isNaN(fuelPrice)) {
+          alert('Please enter valid numbers.');
+          return;
+        }
+        
+        var petrolCost = (distance / 100) * fuelPrice;
+        document.getElementById('result').innerHTML = "Petrol cost: $" + petrolCost.toFixed(2);
+      }
